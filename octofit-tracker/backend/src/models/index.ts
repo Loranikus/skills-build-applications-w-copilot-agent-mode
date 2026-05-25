baba15e18de92e01cb6ad6cde68ccc479567c3c7
Vít Hejný <144330158+Loranikus@users.noreply.github.com>
2026-05-25 14:19:13 +0200

    Revert "Build octofit app"

--- octofit-tracker/backend/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export { ActivityModel } from './Activity';
-export { LeaderboardEntryModel } from './LeaderboardEntry';
-export { TeamModel } from './Team';
-export { UserModel } from './User';
-export { WorkoutModel } from './Workout';
